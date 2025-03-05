8c632f948cfd41790749c408f8c1c2d3d750d114
Nahfiou DJIRE <djirefane@gmail.com>
2025-03-05 15:15:13 +0000

    Rouge a levre 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                     <div class="banner-img">
-                                        <img src="assets/img/banner/1.png" alt="">
+                                        <img src="{{ asset('assets/img/banner/1.png')}}" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -42,7 +42,7 @@
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                     <div class="banner-img">
-                                        <img src="assets/img/banner/2.png" alt="">
+                                        <img src="{{ asset('assets/img/banner/2.png')}}" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -63,7 +63,7 @@
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                     <div class="banner-img">
-                                        <img src="assets/img/banner/3.png" alt="">
+                                        <img src="{{ asset('assets/img/banner/3.png')}}" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -90,7 +90,7 @@
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
                                     <div class="banner-img">
-                                        <img src="assets/img/banner/4.png" alt="">
+                                        <img src="{{ asset('assets/img/banner/4.png')}}" alt="">
                                     </div>
                                 </div>
                             </div>
